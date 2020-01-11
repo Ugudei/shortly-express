05e0e955d9052fea863754b3f58c592c768f2c17
Ugudei <ugudeit@gmail.com>
2020-01-11 11:31:55 -0700

    done with cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,12 +1,26 @@
 const parseCookies = (req, res, next) => {
   // receive the incoming request and access the cookies property
-  conosle.log('request: ', req);
-  // parse them into an object
-  let parsedCookies = JSON.parse(/*cookies fron req */);
-  // assign this object to cookies property on the request.
-  res.cookies = parsedCookies;
 
-  conosle.log('respomd ', res);
+  //console.log('request.headers.cookies: ', req.headers.cookie);
+  // parse them into an object
+  if (req.headers.cookie) {
+    let parsedCookies = JSON.parse(JSON.stringify(req.headers.cookie));
+    let cutUp = parsedCookies.split('; ');
+    let cutUpAll = [];
+    for (let i = 0; i < cutUp.length; i++) {
+      cutUpAll.push(cutUp[i].split('='));
+    }
+    for (let i = 0; i < cutUpAll.length; i++) {
+      var key = cutUpAll[i][0];
+      var value = cutUpAll[i][1];
+      req.cookies[key] = value;
+    }
+  }
+  res.end();
+  next();
+  // res.cookies = parsedCookies;
+
+  //console.log('respond ', res);
 };
 
 module.exports = parseCookies;
